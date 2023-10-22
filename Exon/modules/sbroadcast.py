53784ede8c9522ddd637538ce541ba7661f09626
epic-designer <87082611+epic-designer@users.noreply.github.com>
2023-10-22 14:19:44 -0700

    added forword broadast

--- Exon/modules/sbroadcast.py
+++ Exon/modules/sbroadcast.py
@@ -1,96 +1,64 @@
 import asyncio
-from pyrogram import Client, filters
-from pyrogram.types import Message
 
-#Exon => Your Bots File Name
-import Exon.modules.no_sql.users_db as sql
-from Exon import DEV_USERS, OWNER_ID
-from Exon import Abishnoi as pgram
+from pyrogram import filters
+from pyrogram.enums import ChatMembersFilter
+from pyrogram.errors import FloodWait
+
+from Exon import Abishnoi as app
+from Exon import OWNER_ID as SUDOERS
 from Exon.modules.no_sql.users_db import get_all_users
 
-# get_arg function to retrieve an argument from a message
-def get_arg(message):
-    args = message.text.split(" ")
-    if len(args) > 1:
-        return args[1]
-    else:
-        return None
 
-# Broadcast Function
-@pgram.on_message(filters.command("gbroadcast"))
-async def broadcast_cmd(client: Client, message: Message):
-    user_id = message.from_user.id
-    texttt = message.text.split(" ")
+IS_BROADCASTING = False
 
-    if user_id not in [OWNER_ID] + DEV_USERS:
-        await message.reply_text(
-            "You are not authorized to use this command. Only the owner and authorized users can use it."
-        )
-        return
 
-    if len(texttt) < 2:
-        return await message.reply_text(
-            "<b>BROADCASTING COMMANDS</b>\n-user : broadcasting all user's DM\n-group : broadcasting all groups\n-all : broadcasting both\nEx: /broadcast-user"
-        )
-    
+@app.on_message(filters.command("pbroadcast") & SUDOERS)
+async def braodcast_message(client, message, _):
+    global IS_BROADCASTING
     if message.reply_to_message:
         x = message.reply_to_message.id
         y = message.chat.id
+    else:
+        if len(message.command) < 2:
+            return await message.reply_text(_["broad_2"])
+        query = message.text.split(None, 1)[1]
+        if "-pin" in query:
+            query = query.replace("-pin", "")
 
-    if message.reply_to_message is None and not get_arg(message):
-        return await message.reply_text(
-            "<b>Please provide a message or reply to a message</b>"
-        )
+        if "-user" in query:
+            query = query.replace("-user", "")
+        if query == "":
+            return await message.reply_text(_["broad_8"])
 
-    tex = await message.reply_text("<code>Starting global broadcast...</code>")
+    IS_BROADCASTING = True
+    await message.reply_text(_["broad_1"])
 
-    usersss = 0
-    chatttt = 0
-    uerror = 0
-    cerror = 0
-    chats = sql.get_all_chats() or []
-    users = get_all_users()
-
-    if "-all" in texttt:
-        texttt.append("-user")
-        texttt.append("-group")
-
-    if "-user" in texttt:
-        for chat in users:
-            # if message.reply_to_message:
-            #     x = message.reply_to_message.message_id
-                
-            # else:
-            #     msg = get_arg(message)
+    if "-user" in message.text:
+        susr = 0
+        served_users = []
+        susers = await get_all_users()
+        for user in susers:
+            served_users.append(int(user["user_id"]))
+        for i in served_users:
             try:
-                if message.reply_to_message:
-                    await pgram.forward_messages(int(chat["_id"], y, x))
-                else:
-                    aa = await client.send_message(int(chat["_id"]), msg)
+                m = (
+                    await app.forward_messages(i, y, x)
+                    if message.reply_to_message
+                    else await app.send_message(i, text=query)
+                )
+                susr += 1
+                await asyncio.sleep(0.2)
+            except FloodWait as fw:
+                flood_time = int(fw.value)
+                if flood_time > 200:
+                    continue
+                await asyncio.sleep(flood_time)
+            except:
+                pass
+        try:
+            await message.reply_text(_["broad_4"].format(susr))
+        except:
+            pass
 
-                usersss += 1
-                await asyncio.sleep(0.3)
-            except Exception:
-                uerror += 1
-                await asyncio.sleep(0.3)
-    if "-group" in texttt:
-        for chat in chats:
-            if message.reply_to_message:
-                msg = message.reply_to_message
-            else:
-                msg = get_arg(message)
-            try:
-                if message.reply_to_message:
-                    aa = await msg.copy(int(chat["chat_id"]))
-                else:
-                    aa = await client.send_message(int(chat["chat_id"]))
-
-                chatttt += 1
-                await asyncio.sleep(0.3)
-            except Exception:
-                cerror += 1
-                await asyncio.sleep(0.3)
-
-    await tex.edit_text(
-        f"<b>Message Successfully Sent</b> \nTotal Users: <code>{usersss}</code> \nFailed Users: <code>{uerror}</code> \nTotal GroupChats: <code>{chatttt}</code> \nFailed GroupChats: <code>{cerror}</code>"
-    )
+    
+    IS_BROADCASTING = False
