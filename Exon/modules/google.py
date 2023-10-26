a4fdbb8a70f11cd57e6d7c50bcfb43f4b513c5ec
Tobixy <shivamrajput69bs@gmail.com>
2023-10-26 20:48:02 +0530

    Update google.py

--- Exon/modules/google.py
+++ Exon/modules/google.py
@@ -264,7 +264,7 @@ async def apk(e):
         await e.reply("Exception Occured:- " + str(err))
 
 
-__mod_name__ = "Gᴏᴏɢʟᴇ"
+__mod_name__ = "Gᴏᴏɢʟᴇ 🌐"
 
 __help__ = """
  ❍ /google <text>*:* Perform a google search
