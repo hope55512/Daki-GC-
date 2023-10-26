972d9d73afdaa5a88c498a42c390a5d8582594d1
Tobixy <shivamrajput69bs@gmail.com>
2023-10-26 21:08:54 +0530

    Update paste.py

--- Exon/modules/paste.py
+++ Exon/modules/paste.py
@@ -62,7 +62,7 @@ async def paste_func(_, message):
     return await m.edit(link)
 
 
-__mod_name__ = "Pᴀsᴛᴇ"
+__mod_name__ = "Pᴀsᴛᴇ 🈴"
 __help__ = """
 Pastes the given file and shows you the result
 
