de031372087b39517300d1e7429734c241001c1d
Tobixy <shivamrajput69bs@gmail.com>
2023-10-26 20:41:28 +0530

    Update backups.py

--- Exon/modules/backups.py
+++ Exon/modules/backups.py
@@ -376,7 +376,7 @@ def get_chat(chat_id, chat_data):
         return {"status": False, "value": False}
 
 
-__mod_name__ = "Bᴀᴄᴋᴜᴘ"
+__mod_name__ = "Bᴀᴄᴋᴜᴘ ☁️"
 
 IMPORT_HANDLER = CommandHandler(["import", "backup"], import_data, run_async=True)
 EXPORT_HANDLER = CommandHandler(
