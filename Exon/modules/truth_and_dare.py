d9b201a35b89a822e2b864d81b7b4826f703bcad
Tobixy <shivamrajput69bs@gmail.com>
2023-10-27 07:00:11 +0530

    Update truth_and_dare.py

--- Exon/modules/truth_and_dare.py
+++ Exon/modules/truth_and_dare.py
@@ -54,7 +54,7 @@ async def _(asux):
         await asux.reply(f"ᴇʀʀᴏʀ ʀᴇᴘᴏʀᴛ @{SUPPORT_CHAT}")
 
 
-__mod_name__ = "Tʀᴜᴛʜ-Dᴀʀᴇ"
+__mod_name__ = "Tʀᴜᴛʜ-Dᴀʀᴇ 🧩"
 
 from Exon.modules.language import gs
 
