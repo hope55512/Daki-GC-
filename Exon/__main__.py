8b5b5daf70962e23183736359f933772cf96195b
Tobixy <shivamrajput69bs@gmail.com>
2023-10-28 13:25:05 +0530

    Update __main__.py

--- Exon/__main__.py
+++ Exon/__main__.py
@@ -278,7 +278,7 @@ def start(update: Update, context: CallbackContext):  # sourcery no-metrics
         else:
             first_name = update.effective_user.first_name
             caption = "*Moshi Moshi* {}...".format(escape_markdown(first_name))
-            update.effective_message.reply_text(random.choice(PHOTO), caption=caption, parse_mode=ParseMode.MARKDOWN)
+            update.effective_message.reply_photo(random.choice(PHOTO), caption=caption, parse_mode=ParseMode.MARKDOWN)
 
 
 
