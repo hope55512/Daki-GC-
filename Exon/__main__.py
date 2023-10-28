0f45474c1a5342d9d8550f35f7f4bf19157a9e87
epic-designer <87082611+epic-designer@users.noreply.github.com>
2023-10-28 11:46:57 -0700

    fix pm  start

--- Exon/__main__.py
+++ Exon/__main__.py
@@ -268,11 +268,10 @@ def start(update: Update, context: CallbackContext):  # sourcery no-metrics
 
         else:
             first_name = update.effective_user.first_name
-            # usr = update.effective_user
-            # lol = update.effective_message.reply_text(
-            #     PM_START_TEX.format(usr.first_name), parse_mode=ParseMode.MARKDOWN
-            # )
-            update.effective_message.reply_photo("https://telegra.ph/file/848cf28a2e1dc98e116aa.jpg",caption="Hello  <code>{}</code...").format(escape_markdown(first_name),)
+            caption = "*Hello* {}...".format(escape_markdown(first_name))
+            update.effective_message.reply_photo("https://telegra.ph/file/848cf28a2e1dc98e116aa.jpg", caption=caption, parse_mode=ParseMode.MARKDOWN)
+
+
 
 
             update.effective_message.reply_text(
