8d4733cf68c06f2c1b3596593ef81120c61d5a70
epic-designer <87082611+epic-designer@users.noreply.github.com>
2023-11-02 15:43:49 -0700

    fix error

--- Exon/__init__.py
+++ Exon/__init__.py
@@ -89,7 +89,6 @@ UPDATE_CHAT = Config.UPDATE_CHAT
 MONGO_DB = "Exon"  # DON'T EDIT
 MONGO_PORT = "27017"  # DON'T EDIT
 MONGO_URI = Config.MONGO_URI
-REDIS_URL = Config.REDIS_URL
 DB_NAME = Config.DB_NAME
 BOT_API_URL = "https://api.telegram.org/bot"  # DON'T EDIT
 DB_URL = Config.DATABASE_URL
