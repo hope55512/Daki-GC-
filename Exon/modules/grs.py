160c1a53171e4e7135d1cf7bdfc87ba7815a5024
Kaneki <117969841+Kanekixwof@users.noreply.github.com>
2023-11-20 15:41:32 +0530

    Update grs.py

--- Exon/modules/grs.py
+++ Exon/modules/grs.py
@@ -7,8 +7,8 @@ import requests
 from urllib.parse import quote_plus, unquote
 from bs4 import BeautifulSoup
 from unidecode import unidecode
-from repo import pbot as app
-from repo import bot_token
+from Exon import Abishnoi as app
+from Exon import bot_token
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.enums import ParseMode
