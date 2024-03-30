300eee1efafae277740cd5c54967a1c58a1a75d1
Tobixy <shivamrajput69bs@gmail.com>
2024-03-30 08:54:29 +0530

    Update alive.py

--- Exon/modules/alive.py
+++ Exon/modules/alive.py
@@ -10,7 +10,7 @@ from Exon import BOT_NAME, BOT_USERNAME, OWNER_ID, SUPPORT_CHAT,Abishnoi as pbot
 @pbot.on_message(filters.command("alive"))
 async def awake(_, message: Message):
     TEXT = f"**ʜᴇʏ {message.from_user.mention},\n\n✨ɪ ᴀᴍ {BOT_NAME}**\n▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n"
-    TEXT += f"➢ **ᴍʏ ᴍᴀsᴛᴇʀ :** [Kaneki](tg://user?id={OWNER_ID})\n\n"
+    TEXT += f"➢ **ᴍʏ ᴍᴀsᴛᴇʀ :** [Max 10](tg://user?id={OWNER_ID})\n\n"
     TEXT += f"➢ **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
     TEXT += f"➢ **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
     TEXT += f"➢ **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}`"
